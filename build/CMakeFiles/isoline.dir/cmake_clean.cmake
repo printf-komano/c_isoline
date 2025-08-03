b1657d986f9a7ac529ae73377aa9312849c33af2
Decinf <docblaze160@gmail.com>
2025-08-03 16:51:40 +0300

    added gitignore and main

---
+++ build/CMakeFiles/isoline.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/isoline.dir/link.d"
+  "CMakeFiles/isoline.dir/main.c.o"
+  "CMakeFiles/isoline.dir/main.c.o.d"
+  "isoline"
+  "isoline.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/isoline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/isoline.c
+++ src/isoline.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdint.h>
 
 
 
+/*
+The main goal is to trace a contour (line)
+where function f(x,y) has the certain value
+
+we can start from type definitions
+*/
+
+typedef float il_vec2[2];
+typedef uint32_t il_connection[2];
+
+
+
+typedef struct {
+    il_vec2 scale;
+    il_vec2 offset;
+} il_isoline_config;
+
+
+
+
+
+typedef struct {
+    il_vec2 * points;
+    il_connection * connections;
+} il_isoline_description;
 
 
 
