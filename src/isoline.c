87774bdce6a2c7fb631d6022b690c07b697bbd90
Decinf <docblaze160@gmail.com>
2025-08-03 18:09:15 +0300

    added new functions

--- src/isoline.c
+++ src/isoline.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <stdint.h>
-
+#include <string.h>
 
 
 /*
@@ -15,20 +15,72 @@ typedef float il_vec2[2];
 typedef uint32_t il_connection[2];
 
 
-
+/*
+inital cofing for building isoline.
+*/
 typedef struct {
-    il_vec2 scale;
-    il_vec2 offset;
+    il_vec2 offset; // starting position for calculations
+    il_vec2 scale; // size of the area
+
+    
+    float (*f)(float *, size_t);    // any mathematical function 
+                                    // that returns float value;
+                                    // first 2 parameters are 
+                                    // considered as (x,y)
+    size_t f_param_len;
+
+    float f_border_value; // value where line is going
+    
 } il_isoline_config;
 
 
 
 
-
+/*
+data of the result isoline
+*/
 typedef struct {
     il_vec2 * points;
+    size_t points_len;
+    size_t points_cap;
+
     il_connection * connections;
-} il_isoline_description;
+    size_t connections_len;
+    size_t connections_cap;
+} il_isoline_data;
 
 
 
+
+static float directed_difference(
+        float (*f)(float*, size_t),
+        float * param,
+        size_t param_len,
+        il_vec2 direction
+        ) 
+{
+    
+    float value_start = f(param,param_len);
+
+    float * param_end = (float*) malloc(param_len*sizeof(float));
+    memcpy(param_end, param, param_len*sizeof(float));
+
+    float value_end = f(param_end,param_len);
+
+    return value_end - value_start; //difference between points
+}
+
+
+
+
+void il_get_isoline_data(il_isoline_data * out, il_isoline_config * config);
+
+
+
+
+
+
+
+void il_get_isoline_data(il_isoline_data * out, il_isoline_config * config){
+
+}
