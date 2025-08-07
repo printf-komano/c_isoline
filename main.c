6875d4fc98b216500ed7d90a8ef68e1e4f6b8f9f
Decinf <docblaze160@gmail.com>
2025-08-07 17:37:50 +0300

    correct border detectuion

--- main.c
+++ main.c
@@ -10,15 +10,16 @@
 
 
 float f(il_vec2 arg, float * param, size_t param_len){
-    return (arg[0] + arg[1]) * param[0];
+    return (sinf(arg[0]*param[1]) + sinf(arg[1]*param[1])) * param[0];
 }
 
 
-#define P_LEN 1
+#define P_LEN 2
 
 int main(){
     float * param = (float*) malloc( P_LEN * sizeof(float) );
-    param[0] = 0.1f;
+    param[0] = 1.0f;
+    param[1] = 4.0f;
 
     il_isoline_config cfg;
     cfg.offset[0] = 0.0f; cfg.offset[1] = 0.0f;
@@ -26,7 +27,7 @@ int main(){
     cfg.f = &f;
     cfg.f_param = param;
     cfg.f_param_len = P_LEN;
-    cfg.f_border_value = 0.05f;
+    cfg.f_border_value = 1.0f;
     cfg.grid_len[0] = 10; cfg.grid_len[1] = 10;
 
     il_isoline_data data; 
--- src/isoline.c
+++ src/isoline.c
@@ -24,7 +24,7 @@ typedef struct {
     il_vec2 offset; // starting position for calculations
     il_vec2 scale; // size of the area
 
-    
+
     float (*f)(il_vec2, float *, size_t);   // any mathematical function 
                                             // that returns float value;
                                             // first 2 parameters are 
@@ -46,7 +46,7 @@ typedef struct {
 data of the result isoline
 */
 typedef struct {
-    il_vec2 * points;
+    il_vec2i * points;
     size_t points_len;
     size_t points_cap;
 
@@ -61,9 +61,30 @@ static void push_point(il_vec2 point){
         
 }
 
-static void push_connection(il_connection connection){
 
+static void push_connection(il_connection connection){
+    
 }
+
+
+static bool is_connection_exists(il_isoline_data data, size_t index0, size_t index1){
+    //il_vec2i p = points[index];
+    for(size_t i=0; i<data.connections_len; ++i){
+        if(
+            (data.connections[i][0]==index0 && data.connections[i][1]==index1) ||
+            (data.connections[i][0]==index1 && data.connections[i][1]==index0) ||
+        ){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
+
+
 
 
 
@@ -90,8 +111,10 @@ static inline size_t grid2_to_flat(size_t x, size_t y, size_t x_len){
     return x + y*x_len;
 }
 
+#define GRID2_TO_FLAT(x, y, config) ((y) * (config).grid_len[0] + (x))
 
-#define GRID2_TO_FLAT(x,y,config) (x + y*config.grid_len[0])
+
+
 
 static inline void grid2_to_coordinates(
         size_t x, size_t y, il_isoline_config config, il_vec2 out
@@ -108,20 +131,25 @@ static inline void grid2_to_coordinates(
 
 
 
-static inline bool is_border_value(
+static inline bool is_on_border(
         size_t x, size_t y,
         float * f_values,
         il_isoline_config config
         )
 {
-    bool in_border = 
+    float len_y = config.grid_len[1],
+    len_x = config.grid_len[0];
+
+
+    bool greater = 
         f_values[GRID2_TO_FLAT(x,y,config)] >= config.f_border_value;
-    if(!in_border) return false;
+
+    if(!greater) return false;
     
     //may be on the edge of the grid itself
     if(
-            x <= 0 ||
-            y <= 0 ||
+            x == 0 ||
+            y == 0 ||
             x >= (config.grid_len[0]-1) ||
             y >= (config.grid_len[1]-1)
     ){
@@ -131,10 +159,10 @@ static inline bool is_border_value(
     
     //printf("value matches conditions and not on the edge\n");
     return (
-        f_values[GRID2_TO_FLAT(x+1,y,config)] < config.f_border_value ||
-        f_values[GRID2_TO_FLAT(x-1,y,config)] < config.f_border_value ||
-        f_values[GRID2_TO_FLAT(x,y+1,config)] < config.f_border_value ||
-        f_values[GRID2_TO_FLAT(x,y-1,config)] < config.f_border_value
+        f_values[GRID2_TO_FLAT(x+1, y, config)] < config.f_border_value ||
+        f_values[GRID2_TO_FLAT(x-1, y, config)] < config.f_border_value ||
+        f_values[GRID2_TO_FLAT(x, y+1, config)] < config.f_border_value ||
+        f_values[GRID2_TO_FLAT(x, y-1, config)] < config.f_border_value
     );
     
     
@@ -143,6 +171,8 @@ static inline bool is_border_value(
 
 
 void il_get_isoline_data(il_isoline_data * out, il_isoline_config config){
+
+    il_isoline_data data; 
     
     //first step is to create value grid, where
     float * f_values = (float*) malloc(
@@ -173,23 +203,70 @@ void il_get_isoline_data(il_isoline_data * out, il_isoline_config config){
     }
 
 
-
+    data.points_len = 0;
     //select border points
     for (size_t yi=0; yi<config.grid_len[1]; ++yi){
         for (size_t xi=0; xi<config.grid_len[0]; ++xi){
             il_vec2 point_i;
-            bool bvalue = is_border_value(xi,yi, f_values, config);
+            bool bvalue = is_on_border(xi,yi, f_values, config);
             border_points[ GRID2_TO_FLAT(xi,yi,config) ] = bvalue;
             
+            data.points_len += bvalue;
+
             if(bvalue) printf("o ");
             else printf(". ");
         }
         printf("\n");
     }
 
+    data.points = (il_vec2i*) malloc( data.points_len*sizeof(il_vec2i) );
+    size_t point_count = 0;
+    //il_vec2 pi;
+    //grid2_to_coordinates(0,0,config,pi);
+    
+    for (size_t yi=0; yi<config.grid_len[1]; ++yi){
+        for (size_t xi=0; xi<config.grid_len[0]; ++xi){
+            if(border_points[GRID2_TO_FLAT(xi,yi,config)]){
+                il_vec2i pi;
+                //grid2_to_coordinates(xi,yi,config,pi);
+                printf("%d added point at cords %d,%d\n",
+                    point_count,xi,yi        
+                );
+
+                data.points[point_count][0] = xi;
+                data.points[point_count][1] = yi;
+                
+                ++point_count;
+            }
+        }
+    }
+
+
+
+    //building connections 
+    
+    data.connections_cap = config.grid_len[0] * config.grid_len[1] * 2;
+    data.connections = (il_connection*) 
+        malloc(data.connections_cap * sizeof(il_connection));
+    
+    
+    for(size_t i=0; i<data.points_len; ++i){
+
+        if(is_connection_exists(data,i,j)) continue;
+            if
+        }    
+    }
+
     
 
 
+
+
+    
+
+
+    
+    
 
 
 
